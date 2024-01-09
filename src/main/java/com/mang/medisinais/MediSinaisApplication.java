aa837d1224e30f800964829d9344e9b5e985c072
Thiago Barros <122405319+Bunbismuth@users.noreply.github.com>
2024-01-09 18:53:21 -0300

    Build: Backend project

---
+++ src/main/java/com/mang/medisinais/MediSinaisApplication.java
@@ -0,0 +1,13 @@
+package com.mang.medisinais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSinaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSinaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mang/medisinais/MediSinaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mang.medisinais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediSinaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
