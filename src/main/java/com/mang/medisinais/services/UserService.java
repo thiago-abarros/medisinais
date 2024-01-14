97347453d271c71c05c1c75383ef64651cc11110
Thiago Barros <bunbismuth@proton.me>
2024-01-13 21:27:46 -0300

    feat: created UserService class

---
+++ src/main/java/com/mang/medisinais/services/UserService.java
@@ -0,0 +1,11 @@
+package com.mang.medisinais.services;
+
+import com.mang.medisinais.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+  private final UserRepository userRepo;
+}
