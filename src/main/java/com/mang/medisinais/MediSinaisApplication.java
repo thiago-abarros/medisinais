c4e464a4a934a6ca19d10143070817bc31d0cd54
Thiago Barros <bunbismuth@proton.me>
2024-01-13 21:28:12 -0300

    feat: created MediSinaisApplication interface

--- src/main/java/com/mang/medisinais/MediSinaisApplication.java
+++ src/main/java/com/mang/medisinais/MediSinaisApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MediSinaisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MediSinaisApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MediSinaisApplication.class, args);
+  }
 }
