43e092d72c22a741ec5f401a48ca1ace6c97b67d
Thiago Barros <bunbismuth@proton.me>
2024-01-13 21:26:37 -0300

    feat: created LoginDTO record

---
+++ src/main/java/com/mang/medisinais/domain/dto/LoginDTO.java
@@ -0,0 +1,12 @@
+package com.mang.medisinais.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public record LoginDTO(String email, String password) {}
