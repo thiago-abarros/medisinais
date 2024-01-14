5b89c2c8213dffbc0f77845ad9c189ee876f5b17
Thiago Barros <bunbismuth@proton.me>
2024-01-13 21:25:59 -0300

    feat: created Address class

---
+++ src/main/java/com/mang/medisinais/domain/Address.java
@@ -0,0 +1,32 @@
+package com.mang.medisinais.domain;
+
+import jakarta.persistence.*;
+import java.util.List;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "address_id")
+  private Long id;
+
+  @Column(name = "building_number")
+  private Integer buildingNumber;
+
+  private String cep;
+
+  private String street;
+
+  private String neighbourhood;
+
+  private String city;
+
+  private String state;
+
+  @OneToMany(mappedBy = "user_address")
+  private List<User> users;
+}
