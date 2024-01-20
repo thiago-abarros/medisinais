b991bf7a863a3cbb8913b908dec84a7eff507999
Thiago Barros <bunbismuth@proton.me>
2024-01-20 12:18:06 -0300

    feat: The methods 'registerProfessional' and 'loginProfessional' have been implemented.

--- src/main/java/com/mang/medisinais/controllers/ProfissionalController.java
+++ src/main/java/com/mang/medisinais/controllers/ProfissionalController.java
@@ -1,19 +1,20 @@
 package com.mang.medisinais.controllers;
 
 import com.mang.medisinais.domain.Profissional;
-import com.mang.medisinais.domain.dto.FiltroDTO;
-import com.mang.medisinais.domain.dto.LoginDTO;
-import com.mang.medisinais.domain.dto.ProfissionalDTO;
-import com.mang.medisinais.domain.dto.ResultadoDTO;
+import com.mang.medisinais.dto.FiltroDTO;
+import com.mang.medisinais.dto.LoginDTO;
+import com.mang.medisinais.dto.ProfissionalDTO;
+import com.mang.medisinais.dto.ResultadoDTO;
+import com.mang.medisinais.infra.MediSinaisExcecao;
 import com.mang.medisinais.services.ProfissionalService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,17 +23,27 @@ public class ProfissionalController {
   private final ProfissionalService profissionalService;
 
   @PostMapping("/cadastro")
-  public ResponseEntity<ProfissionalDTO> registrarProfissional(@RequestBody ProfissionalDTO profissionalDTO) {
-    return null;
+  public String cadastrarProfissional
+      (@RequestBody ProfissionalDTO dadosProfissional, ModelMap model) {
+
+    ProfissionalDTO novoProfissional = profissionalService.criarProfissional(dadosProfissional);
+    model.addAttribute("cadastrado", novoProfissional);
+
+    return "paginaCadastrado";
   }
 
   @PostMapping("/login")
-  public RequestEntity<ProfissionalDTO> loginProfissional(@RequestBody LoginDTO login) {
-    return null;
+  public String loginProfissional(@RequestBody LoginDTO login, ModelMap model)
+      throws MediSinaisExcecao {
+
+    Profissional profissionalLogado = profissionalService.autenticarProfissional(login);
+    model.addAttribute("logado", profissionalLogado);
+
+    return "paginaInicial";
   }
 
   @GetMapping("/pesquisa")
-  public String pesquisaProfissionais (FiltroDTO filtroDTO, ModelMap model) {
+  public String pesquisaProfissionais(FiltroDTO filtroDTO, ModelMap model) {
 
     List<Profissional> profissionais = profissionalService.pesquisaProfissionais(filtroDTO);
     model.addAttribute("profissionais", profissionais);
@@ -44,7 +55,7 @@ public class ProfissionalController {
   public String paginaProfissional(@PathVariable String slug, ModelMap model) {
     ResultadoDTO profissional = profissionalService.encontrarPorSlug(slug);
 
-    if(profissional == null) {
+    if (profissional == null) {
       return "erro";
     }
 
