4835bc82929b32cf20e3bd24e836747de2bb11e0
Thiago Barros <bunbismuth@proton.me>
2024-01-20 12:19:04 -0300

    feat: added professional authentication and registration

--- src/main/java/com/mang/medisinais/services/ProfissionalService.java
+++ src/main/java/com/mang/medisinais/services/ProfissionalService.java
@@ -1,42 +1,74 @@
 package com.mang.medisinais.services;
 
 import com.mang.medisinais.domain.Profissional;
-import com.mang.medisinais.domain.dto.FiltroDTO;
-import com.mang.medisinais.domain.dto.ResultadoDTO;
 import com.mang.medisinais.domain.enums.EspecialidadeProfissional;
 import com.mang.medisinais.domain.enums.PlanoSaudeValido;
+import com.mang.medisinais.dto.FiltroDTO;
+import com.mang.medisinais.dto.LoginDTO;
+import com.mang.medisinais.dto.ProfissionalDTO;
+import com.mang.medisinais.dto.ResultadoDTO;
+import com.mang.medisinais.infra.MediSinaisExcecao;
 import com.mang.medisinais.repositories.ProfissionalRepository;
 import com.mang.medisinais.specifications.ProfissionalSpecification;
+import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProfissionalService {
 
+  private static final String EMAIL_PROFISSIONAL_NAO_ENCONTRADO = "O email fornecido, não foi encontrado, por favor forneça um email válido.";
+  private static final String SENHA_INCORRETA = "A senha fornecida está incorreta, verifique se foi digitada corretamente.";
+
   private final ProfissionalRepository userRepo;
+
+  public ProfissionalDTO criarProfissional(ProfissionalDTO dados) {
+
+    if (Boolean.TRUE.equals(validarCadastro(dados))) {
+      Profissional novoProfissional = new Profissional(dados);
+      this.userRepo.save(novoProfissional);
+    }
+
+    return dados;
+  }
 
   public List<Profissional> pesquisaProfissionais(FiltroDTO filtro) {
     var especialidade = EspecialidadeProfissional.valueOfNome(filtro.especialidade());
     var plano = PlanoSaudeValido.valueOfNome(filtro.planoSaude());
 
     return userRepo.findAll(Specification
-            .where(ProfissionalSpecification.temCidade(filtro.cidade())
-            .and(ProfissionalSpecification.temEspecialidade(especialidade)))
-            .and(ProfissionalSpecification.temPlanoSaude(plano)));
+        .where(ProfissionalSpecification.temCidade(filtro.cidade())
+        .and(ProfissionalSpecification.temEspecialidade(especialidade)))
+        .and(ProfissionalSpecification.temPlanoSaude(plano)));
   }
 
   public ResultadoDTO encontrarPorSlug(String slug) {
     Profissional resultado = userRepo.findBySlug(slug);
 
-    if(resultado == null) {
+    if (resultado == null) {
       return null;
     }
 
-    return ResultadoDTO.fromProfissional(userRepo.findBySlug(slug));
+    return ResultadoDTO.fromProfissional(resultado);
   }
 
+  public Profissional autenticarProfissional(LoginDTO dadosLogin) throws MediSinaisExcecao {
+    Profissional profissional = this.userRepo.findByEmail(dadosLogin.email());
+    if (profissional == null) {
+      throw new MediSinaisExcecao(EMAIL_PROFISSIONAL_NAO_ENCONTRADO);
+    }
+    if (!Objects.equals(profissional.getSenha(), dadosLogin.senha())) {
+      throw new MediSinaisExcecao(SENHA_INCORRETA);
+    }
+
+    return profissional;
+  }
+
+  private Boolean validarCadastro(ProfissionalDTO dados) {
+    return !(dados.nome().isEmpty() || dados.cpf().isEmpty() || dados.email().isEmpty()
+        || dados.telefone().isEmpty() || dados.especialidade() == null);
+  }
 }
