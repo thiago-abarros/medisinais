d93f69955e21955fe2c584b13c21f20b639ed231
Thiago Barros <bunbismuth@proton.me>
2024-02-02 20:19:48 -0300

    feat: Validação de CPF e Email
    Também incluso avisos para quando um campo necessário não for preenchido.

--- src/main/java/com/mang/medisinais/domain/Endereco.java
+++ src/main/java/com/mang/medisinais/domain/Endereco.java
@@ -2,6 +2,7 @@ package com.mang.medisinais.domain;
 
 import com.mang.medisinais.domain.enums.UnidadeFederativa;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,26 +19,29 @@ public class Endereco {
   @Column(name = "id_endereco")
   private Long id;
 
+  @NotEmpty(message = "Por favor, preencha o campo de rua")
   @Column(nullable = false)
   private String rua;
 
+  @NotEmpty(message = "Por favor, preencha o campo de bairro")
   @Column(nullable = false)
   private String bairro;
 
+  @NotEmpty(message = "Por favor, preencha o campo de cidade")
   @Column(nullable = false)
   @Size(min = 3, max = 40)
   private String cidade;
 
+  @NotEmpty(message = "Por favor, preencha o campo de UF")
   @Enumerated(EnumType.STRING)
   @Column(nullable = false, length = 2)
   @Size(min = 2, max = 2)
   private UnidadeFederativa uf;
 
+  @NotEmpty(message = "Por favor, preencha o campo de CEP")
   @Column(nullable = false, length = 8)
   @Size(min = 8, max = 8)
   private String cep;
 
-  @ManyToOne
-  private Profissional profissional;
-
+  @ManyToOne private Profissional profissional;
 }
