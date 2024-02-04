9545e538985a56d1dd8c6dbf0d1b39043ae0f778
Thiago Barros <bunbismuth@proton.me>
2024-02-04 10:50:54 -0300

    feat: Adicionada rota alterarProfissional

--- src/main/java/com/mang/medisinais/controllers/ProfissionalController.java
+++ src/main/java/com/mang/medisinais/controllers/ProfissionalController.java
@@ -1,23 +1,29 @@
 package com.mang.medisinais.controllers;
 
 import com.mang.medisinais.domain.Profissional;
-import com.mang.medisinais.dto.*;
+import com.mang.medisinais.dto.AtualizarProfissionalDTO;
+import com.mang.medisinais.dto.CadastroProfissionalDTO;
+import com.mang.medisinais.dto.FiltroDTO;
+import com.mang.medisinais.dto.LoginDTO;
+import com.mang.medisinais.dto.OperacaoDTO;
+import com.mang.medisinais.dto.ResultadoCadastroDTO;
+import com.mang.medisinais.dto.ResultadoDTO;
 import com.mang.medisinais.infra.MediSinaisExcecao;
 import com.mang.medisinais.services.ProfissionalService;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,11 +46,12 @@ public class ProfissionalController {
   }
 
   @PostMapping("/cadastro")
-  public ResponseEntity<Map<String, String>> cadastrarProfissional(CadastroDTO dadosProfissional) {
-    ResultadoCadastroDTO resultadoCadastroDTO = profissionalService.criarProfissional(dadosProfissional);
+  public ResponseEntity<Map<String, String>> cadastrarProfissional(
+      CadastroProfissionalDTO dadosProfissional) {
+    ResultadoCadastroDTO resultadoCadastroDTO = profissionalService.criarProfissional(
+        dadosProfissional);
 
-    if(!resultadoCadastroDTO.status()) {
-      System.out.println(resultadoCadastroDTO.mensagens());
+    if (!resultadoCadastroDTO.status()) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(resultadoCadastroDTO.mensagens());
     }
 
@@ -52,10 +59,10 @@ public class ProfissionalController {
   }
 
   @PostMapping("/login")
-  public ResponseEntity<String> logarProfissional(LoginDTO login, HttpServletRequest request) throws MediSinaisExcecao {
+  public ResponseEntity<String> logarProfissional(LoginDTO login, HttpServletRequest request) {
     OperacaoDTO resultadoLogin = profissionalService.autenticarProfissional(login);
 
-    if(!resultadoLogin.status()) {
+    if (!resultadoLogin.status()) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(resultadoLogin.mensagem());
     }
 
@@ -63,6 +70,19 @@ public class ProfissionalController {
     sessao.setAttribute("idProfissional", resultadoLogin.idProfissional());
 
     return ResponseEntity.status(HttpStatus.OK).build();
+  }
+  @PostMapping("/alterarCadastro")
+  public ResponseEntity<String> alterarProfissional(HttpSession sessao, AtualizarProfissionalDTO dadosProfissional, HttpServletRequest request)
+      throws MediSinaisExcecao {
+
+    Long idProfissional = (Long) sessao.getAttribute("idProfissional");
+    OperacaoDTO resultadoAlteracao = profissionalService.alterarProfissional(idProfissional, dadosProfissional);
+
+    if (!resultadoAlteracao.status()) {
+      return ResponseEntity.status(HttpStatus.NOT_MODIFIED).body(resultadoAlteracao.mensagem());
+    }
+
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
   @GetMapping("/home")
@@ -75,7 +95,8 @@ public class ProfissionalController {
   }
 
   @GetMapping("/pesquisa")
-  public String exibirPesquisaProfissionais(FiltroDTO filtroDTO, Model model, HttpServletRequest request) {
+  public String exibirPesquisaProfissionais(FiltroDTO filtroDTO, Model model,
+      HttpServletRequest request) {
     List<Profissional> profissionais = profissionalService.pesquisaProfissionais(filtroDTO);
     HttpSession sessao = request.getSession(false);
 
@@ -87,8 +108,8 @@ public class ProfissionalController {
 
   @GetMapping("/profissional/{slug}")
   public String exibirPaginaProfissional(@PathVariable String slug, Model model,
-                                         HttpServletRequest request) throws MediSinaisExcecao {
-    ResultadoDTO profissional = profissionalService.encontrarPorSlug(slug);
+      HttpServletRequest request) throws MediSinaisExcecao {
+    ResultadoDTO profissional = profissionalService.encontrarProfissionalPorSlug(slug);
     HttpSession sessao = request.getSession(false);
 
     model.addAttribute("profissional", profissional);
@@ -98,10 +119,11 @@ public class ProfissionalController {
   }
 
   @GetMapping("/logout")
-  public void realizarLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void realizarLogout(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
     HttpSession sessao = request.getSession(false);
 
-    if(sessao != null) {
+    if (sessao != null) {
       sessao.invalidate();
       response.sendRedirect("/");
     }
