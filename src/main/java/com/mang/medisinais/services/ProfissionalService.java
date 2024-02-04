2de7d8c4181cac971db90c3915c21619d8772500
Thiago Barros <bunbismuth@proton.me>
2024-02-04 10:51:17 -0300

    feat: Adicionado alterarProfissional e encontrarProfissionalPorId

--- src/main/java/com/mang/medisinais/services/ProfissionalService.java
+++ src/main/java/com/mang/medisinais/services/ProfissionalService.java
@@ -1,19 +1,25 @@
 package com.mang.medisinais.services;
 
+import com.mang.medisinais.domain.Endereco;
 import com.mang.medisinais.domain.PlanoSaude;
 import com.mang.medisinais.domain.Profissional;
 import com.mang.medisinais.domain.enums.EspecialidadeProfissional;
 import com.mang.medisinais.domain.enums.PlanoSaudeValido;
-import com.mang.medisinais.dto.*;
+import com.mang.medisinais.dto.AtualizarProfissionalDTO;
+import com.mang.medisinais.dto.CadastroProfissionalDTO;
+import com.mang.medisinais.dto.FiltroDTO;
+import com.mang.medisinais.dto.LoginDTO;
+import com.mang.medisinais.dto.OperacaoDTO;
+import com.mang.medisinais.dto.ResultadoCadastroDTO;
+import com.mang.medisinais.dto.ResultadoDTO;
 import com.mang.medisinais.infra.MediSinaisExcecao;
 import com.mang.medisinais.repositories.EnderecoRepository;
 import com.mang.medisinais.repositories.PlanoSaudeRepository;
 import com.mang.medisinais.repositories.ProfissionalRepository;
 import com.mang.medisinais.specifications.ProfissionalSpecification;
-
 import java.util.List;
 import java.util.Map;
-
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,7 +31,7 @@ public class ProfissionalService {
 
   private static final String EMAIL_PROFISSIONAL_NAO_ENCONTRADO = "Email não encontrado.";
   private static final String SENHA_INCORRETA = "Senha incorreta.";
-  private static final String SLUG_NAO_ENCONTRADO = "Profissional não encontrado.";
+  private static final String PROFISSIONAL_NAO_ENCONTRADO = "Profissional não encontrado.";
 
   private final BCryptPasswordEncoder bcrypt;
   private final ProfissionalRepository userRepo;
@@ -33,10 +39,10 @@ public class ProfissionalService {
   private final PlanoSaudeRepository planoRepo;
   private final ValidadorService validador;
 
-  public ResultadoCadastroDTO criarProfissional(CadastroDTO dados) {
+  public ResultadoCadastroDTO criarProfissional(CadastroProfissionalDTO dados) {
     Map<String, String> mensagens = validador.validarProfissional(dados);
 
-    if(!mensagens.isEmpty()) {
+    if (!mensagens.isEmpty()) {
       return new ResultadoCadastroDTO(false, mensagens);
     }
 
@@ -45,13 +51,32 @@ public class ProfissionalService {
     List<PlanoSaude> planos = planoRepo.findAllById(ids);
 
     Profissional novoProfissional = new Profissional(dados, senhaHash, planos);
+    Endereco novoEndereco = new Endereco(dados.endereco(), novoProfissional);
 
     this.userRepo.save(novoProfissional);
-
-    dados.endereco().setProfissional(novoProfissional);
-    enderecoRepo.save(dados.endereco());
+    this.enderecoRepo.save(novoEndereco);
 
     return new ResultadoCadastroDTO(true, mensagens);
+  }
+
+  public OperacaoDTO alterarProfissional(Long idProfissional, AtualizarProfissionalDTO dados)
+      throws MediSinaisExcecao {
+    // TODO Validação
+
+    Profissional profissional = encontrarProfissionalPorId(idProfissional);
+    Endereco endereco = new Endereco(dados.endereco(), profissional);
+    String senhaHash = bcrypt.encode(dados.senha());
+
+    profissional.setNome(dados.nome());
+    profissional.setFoto(dados.foto());
+    profissional.setEmail(dados.email());
+    profissional.setPlanosAceitos(dados.planosAceitos());
+    profissional.setSenha(senhaHash);
+
+    this.userRepo.save(profissional);
+    this.enderecoRepo.save(endereco);
+
+    return new OperacaoDTO(true, "Dados alterados com sucesso", idProfissional);
   }
 
   public List<Profissional> pesquisaProfissionais(FiltroDTO filtro) {
@@ -60,18 +85,28 @@ public class ProfissionalService {
 
     return userRepo.findAll(Specification
         .where(ProfissionalSpecification.temCidade(filtro.cidade())
-        .and(ProfissionalSpecification.temEspecialidade(especialidade)))
+            .and(ProfissionalSpecification.temEspecialidade(especialidade)))
         .and(ProfissionalSpecification.temPlanoSaude(plano)));
   }
 
-  public ResultadoDTO encontrarPorSlug(String slug) throws MediSinaisExcecao {
+  public ResultadoDTO encontrarProfissionalPorSlug(String slug) throws MediSinaisExcecao {
     Profissional resultado = userRepo.findBySlug(slug);
 
     if (resultado == null) {
-      throw new MediSinaisExcecao(SLUG_NAO_ENCONTRADO);
+      throw new MediSinaisExcecao(PROFISSIONAL_NAO_ENCONTRADO);
     }
 
     return ResultadoDTO.fromProfissional(resultado);
+  }
+
+  public Profissional encontrarProfissionalPorId(Long id) throws MediSinaisExcecao {
+    Optional<Profissional> resultado = userRepo.findById(id);
+
+    if (resultado.isEmpty()) {
+      throw new MediSinaisExcecao(PROFISSIONAL_NAO_ENCONTRADO);
+    }
+
+    return resultado.get();
   }
 
   public OperacaoDTO autenticarProfissional(LoginDTO dadosLogin) {
