1bd6a5575341ae315a771f718d60471a9e39e919
João Neto <jbsn1@discente.ifpe.edu.br>
2024-02-08 19:12:47 -0300

    fix: modal de login na lista de profissionais

--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -1,15 +1,36 @@
-  document.getElementById('loginForm').addEventListener('submit', (event) => {
-      event.preventDefault();
+const wrapper = document.querySelector('.wrapper');
+const loginPopup = document.querySelector('.login-btn');
+const closeIcon = document.querySelector('.icon-close');
 
-      const formData = new FormData(event.target);
+function toggleActiveClass() {
+    if (wrapper.classList.contains('active')) {
+        wrapper.classList.remove('active');
+    }
+}
 
-      axios.post('/login', formData)
-        .then(function (response) {
-          window.location.href = '/';
-        })
-        .catch(function (error) {
-          const msgErro = document.getElementById('msgErro');
-          msgErro.textContent = error.response.data;
-          msgErro.style.display = 'block';
-        });
-  });
+loginPopup.addEventListener('click', ()=> {
+    wrapper.id = '';
+    wrapper.classList.add('active-login');
+    wrapper.classList.add('open');
+});
+
+
+closeIcon.addEventListener('click', ()=> {
+    wrapper.id = 'close';
+    wrapper.classList.remove('open');
+    toggleActiveClass();
+});
+
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    axios.post('/login', formData).then(function (response) {
+        window.location.href = '/';
+    }).catch(function (error) {
+        const msgErro = document.getElementById('msgErro');
+        msgErro.textContent = error.response.data;
+        msgErro.style.display = 'block';
+    });
+});
